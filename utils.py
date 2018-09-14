00400310cbf3f0a0df728ddd6ee9185e706dc9b9
seoilgun <seoilgun@gmail.com>
2018-09-14 15:31:17 +0900

    summary change

--- utils.py
+++ utils.py
@@ -11,6 +11,7 @@ from tqdm import trange
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 
+
 def maybe_download_and_extract(dir_path, url_name, is_tarfile=False, is_zipfile=False):
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
@@ -156,18 +157,19 @@ def unprocess_image(image, mean_pixel, norm):
 
 def add_to_regularization_and_summary(var):
     if var is not None:
-        tf.histogram_summary(var.op.name, var)
+        tf.summary.histogram(var.op.name, var)
         tf.add_to_collection("reg_loss", tf.nn.l2_loss(var))
 
 
 def add_activation_summary(var):
-    tf.histogram_summary(var.op.name + "/activation", var)
-    tf.scalar_summary(var.op.name + "/sparsity", tf.nn.zero_fraction(var))
+    tf.summary.histogram(var.op.name + "/activation", var)
+    tf.summary.scalar(var.op.name + "/sparsity", tf.nn.zero_fraction(var))
 
 
 def add_gradient_summary(grad, var):
     if grad is not None:
-        tf.histogram_summary(var.op.name + "/gradient", grad)
+        tf.summary.histogram(var.op.name + "/gradient", grad)
+
 
 def save_imshow_grid(images, logs_dir, filename, shape):
     """
