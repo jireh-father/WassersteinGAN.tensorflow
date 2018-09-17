ee03980e7493d45cf365dc940a5732fbe35fc055
seoilgun <seoilgun@gmail.com>
2018-09-17 16:27:08 +0900

    summary change

--- run_main_ce.sh
+++ run_main_ce.sh
@@ -1,1 +1,1 @@
-python main.py --logs_dir=logs/CelebA_WGAN_logs2/ --optimizer=Adam --learning_rate=2e-4 --optimizer_param=0.9 --model=0 --iterations=2e4 --mode=train --data_dir=/home/data/celeba
+python main.py --logs_dir=logs/CelebA_WGAN_logs2/ --optimizer=Adam --learning_rate=2e-4 --optimizer_param=0.9 --model=0 --iterations=2e8 --mode=train --data_dir=/home/data/celeba
